b250590c22959a06b111bdedb64fdcccafc3df9a
Nadav Yigal <nadav@runsmart.dev>
2026-04-28 12:20:50 +0300

    Initial Commit

---
+++ IOS RunSmart app/IOS_RunSmart_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_RunSmart_appApp.swift
+//  IOS RunSmart app
+//
+//  Created by Nadav Yigal on 28/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct IOS_RunSmart_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
